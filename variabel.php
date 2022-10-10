1f3f8810e0a27971a2e5fa69c2a4669b7221b927
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-10-10 09:12:41 +0700

    Create variabel.php

---
+++ variabel.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "Muhammad Amar Dafi";
+$jurusan = "Teknik Informatika";
+$umur = 21;
+$ipk = 4.0;
+$isMarried = false;
+
+echo $nama;
+echo "<br>";
+echo $jurusan;
+echo "<br>";
+echo $umur;
+echo "<br>";
+echo $ipk;
+echo "<br>";
+echo ($isMarried === true)? "Sudah Menikah" : "Belum Menikah";
