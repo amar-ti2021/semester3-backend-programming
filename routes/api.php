46504c6b226bd77f342d062cbb058e14b6d5960e
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-11-09 13:07:46 +0700

    Add new endpoin to get student's detail

--- routes/api.php
+++ routes/api.php
@@ -22,11 +22,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/animals', [AnimalController::class, 'index']);
-Route::post('/animals',[AnimalController::class, 'store']);
-Route::put('/animals/{id}',[AnimalController::class, 'update']);
-Route::delete('/animals/{id}',[AnimalController::class, 'destroy']);
+Route::post('/animals', [AnimalController::class, 'store']);
+Route::put('/animals/{id}', [AnimalController::class, 'update']);
+Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
 
 Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{id}', [StudentController::class, 'show']);
 Route::post('/students', [StudentController::class, 'store']);
 Route::put('/students/{id}', [StudentController::class, 'update']);
 Route::delete('/students/{id}', [StudentController::class, 'destroy']);
