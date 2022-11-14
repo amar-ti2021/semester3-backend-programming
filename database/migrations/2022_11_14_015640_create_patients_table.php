ddd1dc336b16428805e8b2a2165a33cf7519a64e
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-11-14 09:40:10 +0700

    Add new columns and make several comments
    Add a comment to describe id column
    Add a comment to describe timestamp column
    Add column name and a comment to describe it
    Add column phone and a comment to describe it
    Add column address and a comment to describe it
    Add column status_id and a comment to describe it
    Add column in_date_at and a comment to describe it
    Add column status_id and a comment to describe it
    Add column out_date_at and a comment to describe it

--- database/migrations/2022_11_14_015640_create_patients_table.php
+++ database/migrations/2022_11_14_015640_create_patients_table.php
@@ -14,7 +14,30 @@ return new class extends Migration
     public function up()
     {
         Schema::create('patients', function (Blueprint $table) {
+
+            // ID Pasien
             $table->id();
+
+            // Nama Pasien
+            $table->string('name');
+
+            // Nomor Telepon Pasien 
+            $table->string('phone');
+
+            // Alamat Pasien 
+            $table->text('address');
+
+            // Status pasien, foreign key dari table statuses 
+            // 0 = positive, 1 = recovered, 2 = dead
+            $table->foreignId('status_id');
+
+            // Tanggal pasien terkonfirmasi Covid 
+            $table->date('in_date_at');
+
+            // Tanggal pasien sembuh atau meninggal, null jika pasien masih dirawat
+            $table->date('out_date_at')->nullable();
+
+            // Tanggal dibuatnya data 
             $table->timestamps();
         });
     }
