84bc9a419e3773e2b39033e9f6225469230c7847
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-12-19 10:07:01 +0700

    Update index method at StudentController

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -1,6 +1,9 @@
-const students = require("../data/students");
+const Student = require("../models/Student");
+
 class StudentController {
-  index(req, res) {
+  async index(req, res) {
+    const students = await Student.all();
+
     const data = {
       message: "Menampilkan semua students",
       data: students,
@@ -8,36 +11,36 @@ class StudentController {
     res.json(data);
   }
 
-  store(req, res) {
-    const { nama } = req.body;
-    students.push(nama);
-    const data = {
-      message: `Menambahkan data student : ${nama}`,
-      data: students,
-    };
-    res.json(data);
-  }
+  // store(req, res) {
+  //   const { nama } = req.body;
+  //   students.push(nama);
+  //   const data = {
+  //     message: `Menambahkan data student : ${nama}`,
+  //     data: students,
+  //   };
+  //   res.json(data);
+  // }
 
-  update(req, res) {
-    const { id } = req.params;
-    const { nama } = req.body;
-    students[id] = nama;
-    const data = {
-      message: `Mengedit student ${id}, nama ${nama}`,
-      data: students,
-    };
-    res.json(data);
-  }
+  // update(req, res) {
+  //   const { id } = req.params;
+  //   const { nama } = req.body;
+  //   students[id] = nama;
+  //   const data = {
+  //     message: `Mengedit student ${id}, nama ${nama}`,
+  //     data: students,
+  //   };
+  //   res.json(data);
+  // }
 
-  destroy(req, res) {
-    const { id } = req.params;
-    students.splice(id, 1);
-    const data = {
-      message: `Menghapus student id ${id}`,
-      data: students,
-    };
-    res.json(data);
-  }
+  // destroy(req, res) {
+  //   const { id } = req.params;
+  //   students.splice(id, 1);
+  //   const data = {
+  //     message: `Menghapus student id ${id}`,
+  //     data: students,
+  //   };
+  //   res.json(data);
+  // }
 }
 
 const object = new StudentController();
