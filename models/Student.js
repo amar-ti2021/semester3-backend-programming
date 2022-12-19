25892c257a91f332328ac055d761c6a3ba69bef8
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-12-19 21:27:11 +0700

    Add method save, find, and delete

--- models/Student.js
+++ models/Student.js
@@ -10,6 +10,44 @@ class Student {
       });
     });
   }
+  static save(data) {
+    return new Promise((resolve, reject) => {
+      const query = "Insert INTO students SET ?";
+
+      db.query(query, data, (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
+  static find(id) {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM students WHERE id=${id}`;
+      db.query(query, (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  }
+  static async delete(id) {
+    try {
+      const data = await this.find(id);
+      return new Promise((resolve, reject) => {
+        const query = `DELETE FROM students WHERE id=${id}`;
+        db.query(query, (error, results) => {
+          resolve(data);
+        });
+      });
+    } catch (error) {
+      return `Error : ${error}`;
+    }
+  }
 }
 
 module.exports = Student;
