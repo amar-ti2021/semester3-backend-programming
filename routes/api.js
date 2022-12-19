3f19a4d9c67330449f44e174fe552ba1520e81c1
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-12-19 21:27:40 +0700

    Add API endpoints post students, and del students

--- routes/api.js
+++ routes/api.js
@@ -9,10 +9,10 @@ router.get("/", (req, res) => {
 
 router.get("/students", StudentController.index);
 
-// router.post("/students", StudentController.store);
+router.post("/students", StudentController.store);
 
 // router.put("/students/:id", StudentController.update);
 
-// router.delete("/students/:id", StudentController.destroy);
+router.delete("/students/:id", StudentController.destroy);
 
 module.exports = router;
